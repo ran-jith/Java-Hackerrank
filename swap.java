40a176ba5cad94f2299e582867483e6bdc44b5d2
MAHESH NAIR <43637836+maheshmnair@users.noreply.github.com>
2020-10-23 11:58:09 +0530

    swap problem

---
+++ swap.java
@@ -0,0 +1,18 @@
+import java.io.*;
+ 
+class swap {
+    public static void main(String[] args)
+    {
+        int x;
+        int y;
+ 
+        // Code to swap 'x' and 'y'
+        x = x * y; 
+        y = x / y; 
+        x = x / y; 
+ 
+        System.out.println("After swaping:"
+                           + " x = " + x + ", y = " + y);
+    }
+}
+ 
